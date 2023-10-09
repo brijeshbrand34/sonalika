1bda27d5cce1b353812b47f2297fdb542bb4faad
shashank562000 <shashank562000@gmail.com>
2023-10-09 17:51:25 +0530

    whichlist issue slove

--- router/WhichList.js
+++ router/WhichList.js
@@ -5,43 +5,46 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const path = require('path');
 
-const WhichList = require('../model/WhichListSchema');
+const WhichLists = require('../model/WhichListSchema');
 
 router.post('/CreateWhichlist', async (req, res) => {
-    const {productId , Userid} =req.body
-    try{
-        if(!productId || !Userid){
-            res.json("productId  Userid is not find")
-        }
-        WhichLists = new WhichList({
-            UserId:Userid,
-            ProductId:productId
-        })
-        console.log(WhichLists)
-        WhichListsSave= await WhichLists.save();
+    const { productId, UserId } = req.body;
+
+    if (!productId || !UserId) {
+        return res.status(400).json({ error: "productId or UserId is not provided" });
+    }
+
+    try {
+        const WhichList = new WhichLists({
+            UserId: UserId,
+            ProductId: productId
+        });
+
+        const WhichListsSave = await WhichList.save();
         res.json(WhichListsSave);
-    }catch(error){
+    } catch (err) {
+        console.error(err);
         res.status(500).json({ error: 'Internal server error' });
     }
-})
+});
 
 router.get('/ReviewGetOne/:userId', async (req, res) => {
     const userId = req.params.userId;
     console.log("getOne", userId)
     console.log("get", req.params.userId)
     try {
-      const WhichList = await WhichList.find({ UserId: userId }); // Fetch the Review based on the provided ID
-  
-      if (!WhichList) {
-        return res.status(404).json({ error: "WhichList not found" });
-      }
-  
-      console.log("WhichList information for ID", UserId, ":", WhichList);
-  
-      res.json({ WhichList }); // Send the Review as JSON response
+        const WhichList = await WhichList.find({ UserId: userId }); // Fetch the Review based on the provided ID
+
+        if (!WhichList) {
+            return res.status(404).json({ error: "WhichList not found" });
+        }
+
+        console.log("WhichList information for ID", UserId, ":", WhichList);
+
+        res.json({ WhichList }); // Send the Review as JSON response
     } catch (error) {
-      console.error("Error fetching WhichList:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+        console.error("Error fetching WhichList:", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
-  });
-  module.exports = router;
+});
+module.exports = router;
